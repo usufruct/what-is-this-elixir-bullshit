7ccc632da5d857ff28c168cef01b1d9283d9ec0d
manderson <manderson@leapfrogonline.com>
2015-10-21 15:06:18 -0500

    wip (don't look, it's terrible)

--- insort/lib/insort.ex
+++ insort/lib/insort.ex
@@ -1,5 +1,17 @@
 defmodule Insertion do
   def sort(string) do
-    string
+    sort_points([], String.codepoints(string))
   end
+
+  defp sort_points(accum, [head, tail]) do
+    IO.puts "hi"
+    IO.puts head
+    sort_points(accum, tail)
+  end
+
+  defp sort_points(x, y) do
+    #IO.puts x
+    IO.puts y
+  end
+
 end
