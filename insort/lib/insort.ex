de2afa025783bc168b65ef7aa3d6acf7f2a36376
manderson <manderson@leapfrogonline.com>
2015-10-22 12:54:26 -0500

    test passes again

--- insort/lib/insort.ex
+++ insort/lib/insort.ex
@@ -1,17 +1,23 @@
 defmodule Insertion do
   def sort(string) do
-    sort_points([], String.codepoints(string))
+    sort_points("", String.codepoints(string))
   end
 
-  defp sort_points(accum, [head, tail]) do
-    IO.puts "hi"
-    IO.puts head
-    sort_points(accum, tail)
+  defp sort_points(accum, [head | tail]) do
+    insert_index = Enum.find_index(String.codepoints(accum), fn(sorted_codepoint) ->
+      sorted_codepoint > head
+    end)
+
+    case {insert_index} do
+      {nil} ->
+        sort_points(accum <> head, tail)
+      _ ->
+        sort_points(head <> accum, tail)
+    end
   end
 
-  defp sort_points(x, y) do
-    #IO.puts x
-    IO.puts y
+  defp sort_points(accum, []) do
+    accum
   end
 
 end
