1adc683d78b1d3a95c6c969e80a0fe2c82614bb5
Michael A Anderson <usufruct@gmail.com>
2015-11-01 15:30:45 -0600

    tests pass, sort happens

--- tresort/lib/tresort.ex
+++ tresort/lib/tresort.ex
@@ -7,4 +7,31 @@ defmodule Insertion do
 		IO.puts Placer
 		Placer.place_the_truth
 	end
+
+	@spec insort(bitstring) :: bitstring
+	def insort(unsorted) do
+		sort_codepoints([], String.codepoints(unsorted))
+	end
+
+	@spec sort_codepoints(list, list) :: bitstring
+	defp sort_codepoints([], []) do
+		""
+	end
+
+	@spec sort_codepoints(list, list) :: bitstring
+	defp sort_codepoints(sorted, [item | items_to_sort]) do
+		# iex(1)> "a" < "b"
+		# true
+		# iex(2)> "b" < "a"
+		# false
+		insert_point = Enum.find_index(sorted, fn(sorted_item) -> item < sorted_item end)
+
+		Placer.place(sorted, item, insert_point)
+		|> sort_codepoints(items_to_sort)
+	end
+
+	@spec sort_codepoints(list, list) :: bitstring
+	defp sort_codepoints(sorted, []) do
+		Enum.join(sorted)
+	end
 end
--- tresort/test/tresort_test.exs
+++ tresort/test/tresort_test.exs
@@ -10,5 +10,31 @@ defmodule InsertionTest do
   	assert Insertion.insert_place_true == true
   end
 
+	test "insort sorts empty string" do
+		assert Insertion.insort("") == ""
+	end
 
+	test "insort sorts one-letter strings" do
+		assert Insertion.insort("a") == "a"
+		assert Insertion.insort("b") == "b"
+	end
+
+	test "insort sorts two-letter strings" do
+		assert Insertion.insort("ab") == "ab"
+		assert Insertion.insort("ba") == "ab"
+	end
+
+	test "insort sorts three-letter strings" do
+		assert Insertion.insort("abc") == "abc"
+		assert Insertion.insort("cab") == "abc"
+	end
+
+	test "insort sorts duplicates correctly" do
+		assert Insertion.insort("abb") == "abb"
+		assert Insertion.insort("bab") == "abb"
+	end
+
+	test "insort likes Genesis" do
+		assert Insertion.insort("abacab") == "aaabbc"
+	end
 end
